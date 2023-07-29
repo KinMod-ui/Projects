9767d067572ec84e8b8d41be3e93ac8cc38bf656
Pratham <54993472+KinMod-ui@users.noreply.github.com>
2023-07-29 14:30:32 +0530

    Update yt_api.py

--- GIT/APIS/yt_api.py
+++ GIT/APIS/yt_api.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-api_key = 'AIzaSyB-7iXZ-s47U2PsWMtXF6KToWe24L_0u20'
+
 from apiclient.discovery import build
 
 youtube = build('youtube' , 'v3' , developerKey=api_key)
